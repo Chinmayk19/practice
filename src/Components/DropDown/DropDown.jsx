53f24f83ec956b045c28f6a8ab305756069219c0
ChinmayK19 <ckulkarni198@gmail.com>
2024-07-17 18:07:20 +0530

    fourth commit

--- src/Components/DropDown/DropDown.jsx
+++ src/Components/DropDown/DropDown.jsx
@@ -1,20 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import "./DropDown.css"
 import { IoMdArrowDropdown } from "react-icons/io";
 
 
 const Dropdown = ({ label, options, value, onChange }) => {
+    const [focus,setFocus]=useState("0deg");
+    function selectFocus(){
+        setFocus("180deg")
+    }
+    function looseFocus(){
+        setFocus("0deg")
+    }
   return (
     <div className="search-container">
       <label>{label}</label>
-      <select value={value} onChange={(e) => onChange(e.target.value)} className="custom-select">
+      <select value={value} onChange={(e) => onChange(e.target.value)} className="custom-select" onFocus={selectFocus} onBlur={looseFocus}>
         {options.map((option, index) => (
           <option key={index} value={option}> 
             {option}
           </option>
         ))}
       </select>
-      <div className="dropdown-arrow">
+      <div className="dropdown-arrow" style={{transform:`rotate(${focus})`}}>
       <IoMdArrowDropdown />
       </div>
     </div>
